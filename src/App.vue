5bcaa5e15ccaac75c61486ba0beadf5f17175623
seolranlee <leesugar90@gmail.com>
2022-02-21 21:59:06 +0900

    post area mobile ver develop

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,202 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <button
-      class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700"
-    >
-      Hello, Tailwind CSS!
-    </button>
+    <HelloWorld v-show="false" msg="Welcome to Your Vue.js App" />
 
+    <!-- blog  -->
+    <div class="wrapper p-4">
+      <ul class="posts">
+        <li class="item py-8 border-t border-gray-100">
+          <div>
+            <h3 class="font-sans text-base font-medium text-primary">Tech</h3>
+            <h2
+              class="font-sans text-2xl font-bold text-black line-clamp-3 md:line-clamp-2"
+            >
+              <a href="#">애자일의 근본 알아보기</a>
+            </h2>
+            <p>
+              <span class="date text-sm text-gray-400">2021.12.20</span>
+              <span class="author text-sm text-gray-400"
+                ><a href="#">Jimmy</a></span
+              >
+            </p>
+            <ul class="tag flex flex-wrap mt-8 gap-2">
+              <li>
+                <a href="#">
+                  <div
+                    class="rounded-2xl py-1 px-3 border border-gray-200 text-tag leading-tag"
+                  >
+                    #DesignSystem
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div
+                    class="rounded-2xl py-1 px-3 border border-gray-200 text-tag leading-tag"
+                  >
+                    #Agile
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div
+                    class="rounded-2xl py-1 px-3 border border-gray-200 text-tag leading-tag"
+                  >
+                    #CI/CD
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li class="item py-8 border-t border-gray-100">
+          <div>
+            <h3 class="font-sans text-base font-medium text-primary">Tech</h3>
+            <h2
+              class="font-sans text-2xl font-bold text-black line-clamp-3 md:line-clamp-2"
+            >
+              <a href="#"
+                >애자일의 근본 알아보기 애자일의 근본 알아보기 애자일의 근본
+                알아보기 애자일의 근본 알아보기 애자일의 근본 알아보기 애자일의
+                근본 알아보기</a
+              >
+            </h2>
+            <p>
+              <span class="date text-sm text-gray-400">2021.12.20</span>
+              <span class="author text-sm text-gray-400"
+                ><a href="#">Jimmy</a></span
+              >
+            </p>
+            <ul class="tag flex flex-wrap mt-8 gap-2">
+              <li>
+                <a href="#">
+                  <div
+                    class="rounded-2xl py-1 px-3 border border-gray-200 text-tag leading-tag"
+                  >
+                    #DesignSystem
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div
+                    class="rounded-2xl py-1 px-3 border border-gray-200 text-tag leading-tag"
+                  >
+                    #Agile
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div
+                    class="rounded-2xl py-1 px-3 border border-gray-200 text-tag leading-tag"
+                  >
+                    #CI/CD
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div
+                    class="rounded-2xl py-1 px-3 border border-gray-200 text-tag leading-tag"
+                  >
+                    #DesignSystem
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div
+                    class="rounded-2xl py-1 px-3 border border-gray-200 text-tag leading-tag"
+                  >
+                    #Agile
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div
+                    class="rounded-2xl py-1 px-3 border border-gray-200 text-tag leading-tag"
+                  >
+                    #CI/CD
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li class="item py-8 border-t border-gray-100">
+          <div>
+            <h3 class="font-sans text-base font-medium text-primary">Design</h3>
+            <h2
+              class="font-sans text-2xl font-bold text-black line-clamp-3 md:line-clamp-2"
+            >
+              <a href="#">숨고 디자인 시스템 제작 스토리</a>
+            </h2>
+            <p>
+              <span class="date text-sm text-gray-400">2021.11.20</span>
+              <span class="author text-sm text-gray-400"
+                ><a href="#">Ken</a></span
+              >
+            </p>
+            <ul class="tag flex flex-wrap mt-8 gap-2">
+              <li>
+                <a href="#">
+                  <div
+                    class="rounded-2xl py-1 px-3 border border-gray-200 text-tag leading-tag"
+                  >
+                    #DesignSystem
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div
+                    class="rounded-2xl py-1 px-3 border border-gray-200 text-tag leading-tag"
+                  >
+                    #Agile
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div
+                    class="rounded-2xl py-1 px-3 border border-gray-200 text-tag leading-tag"
+                  >
+                    #Platform
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
-<style>
-@import './assets/index.css';
+<style lang="scss" scoped>
+@import "./assets/index.css";
+.posts {
+  .item {
+    .date {
+      &::after {
+        content: "ㅣ";
+        color: #e2e2e2;
+      }
+    }
+  }
+}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,28 @@
 module.exports = {
-  purge: [
-    './public/**/*.html',
-    './src/**/*.vue',
-  ],
+  purge: ["./public/**/*.html", "./src/**/*.vue"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Noto Sans KR", "Malgun Gothic", "sans-serif"],
+    },
+    extend: {
+      lineHeight: {
+        tag: "1.58",
+      },
+      textColor: {
+        primary: "#00c7ae",
+        tag: "#888888",
+      },
+    },
+    // textColor: {
+    //   'primary': '#00c7ae',
+    //   'title': '#2d2d2d',
+    //   'gray-200': '#e2e2e2',
+    //   'gray-400': '#a9a9a9'
+    // }
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 };
